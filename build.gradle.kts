ed1f047b271bc8f72412363dcf19362e04ba1ec7
Cameron Kirk <kirkster96@gmail.com>
2024-12-01 15:22:18 -0800

    add Day 1 benchmark

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     kotlin("jvm") version "2.1.0"
+    id("org.jetbrains.kotlinx.benchmark") version "0.4.13"
+    kotlin("plugin.allopen") version "2.0.20"
+
+}
+allOpen {
+    annotation("org.openjdk.jmh.annotations.State")
 }
 
 sourceSets {
@@ -8,8 +14,24 @@ sourceSets {
     }
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.13")
+}
+
+benchmark {
+    targets {
+        register("main")
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
 tasks {
     wrapper {
         gradleVersion = "8.11.1"
     }
 }
+
+
