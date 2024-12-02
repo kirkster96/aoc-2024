0d5395b9ac01cd80f413da638ece9e3add7d6e82
Cameron Kirk <kirkster96@gmail.com>
2024-12-01 22:31:56 -0800

    move Day 1 part 1 and part 2 function scopes out from global so I can do Day 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,42 +2,45 @@ import kotlin.io.path.Path
 import kotlin.io.path.inputStream
 import kotlin.io.path.reader
 
-fun distance(a:Int, b:Int):Int {
-    if (a == b) return 0
-    else if (a > b) return a - b
-    else return b - a
-}
-fun part1(l: List<Int>,r: List<Int>): Int {
 
-    val listPairs = l.sorted().zip(r.sorted())
-
-    val result: Int = listPairs.map { distance(it.first, it.second) }.reduce { acc, i -> acc + i }
-
-    return result
-}
-
-fun part2(l: List<Int>,r: List<Int>): Int {
-    var rCounts: MutableMap<Int, Int>  = r.sorted()
-        .distinct()
-        .map { mutableMapOf<Int, Int>(Pair(it, 0)) }
-        .reduce { acc, mutableMap -> run {
-            acc.putAll(mutableMap)
-            acc
-        }}
-
-    r.sorted().forEach {  run {
-        val x = rCounts.getValue(it)
-        rCounts[it] = x + 1
-    } }
-
-    val result = l.sorted()
-        .map { it * rCounts.getOrDefault(it, 0) }   // count * multiplier
-        .reduce { acc, i -> acc + i }
-
-    return result
-}
 
 fun main() {
+
+    fun distance(a:Int, b:Int):Int {
+        if (a == b) return 0
+        else if (a > b) return a - b
+        else return b - a
+    }
+
+    fun part1(l: List<Int>,r: List<Int>): Int {
+
+        val listPairs = l.sorted().zip(r.sorted())
+
+        val result: Int = listPairs.map { distance(it.first, it.second) }.reduce { acc, i -> acc + i }
+
+        return result
+    }
+
+    fun part2(l: List<Int>,r: List<Int>): Int {
+        var rCounts: MutableMap<Int, Int>  = r.sorted()
+            .distinct()
+            .map { mutableMapOf<Int, Int>(Pair(it, 0)) }
+            .reduce { acc, mutableMap -> run {
+                acc.putAll(mutableMap)
+                acc
+            }}
+
+        r.sorted().forEach {  run {
+            val x = rCounts.getValue(it)
+            rCounts[it] = x + 1
+        } }
+
+        val result = l.sorted()
+            .map { it * rCounts.getOrDefault(it, 0) }   // count * multiplier
+            .reduce { acc, i -> acc + i }
+
+        return result
+    }
 
     // Read the input from the `src/Day01.txt` file.
     var group1 = listOf<Int>()
